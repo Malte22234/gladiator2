70ab926f1a3865dcdca0b725dcf00ed647ccdf8e
Malte22234 <malte.bergstrom@elev.ga.ntig.se>
2024-10-11 15:13:14 +0200

    Add files via upload

--- gladiator.py
+++ gladiator.py
@@ -1,6 +1,7 @@
 import random
 print("Du är en gladiator utan vapen som ska slåss i en gladiator arena din motståndare gandalf är en mycket erfaren och farlig motståndare var försiktigt med honom. ")
 spelarens_val=input("Välj mellan A,B och C för att attackera").lower()
+
 if (spelarens_val=="a"):
     print("spelaren valde valde A")
 elif (spelarens_val=="b"):
@@ -13,6 +14,7 @@ spelarens_hp=3
 Gandalf1_hp=3
 
 Gandalf1=random.randint(1, 3)
+
 if (Gandalf1==1 and spelarens_val == "b"):
     print("Din motståndare valde attack A och du valde attack B. Du förlorar 1 hp.")
     spelarens_hp = spelarens_hp - 1
@@ -24,3 +26,29 @@ elif(Gandalf1==3 and spelarens_val == "c"):
 elif(Gandalf1==2 and spelarens_val == "c"):
     print("din motståndare valde attack b och du valde attack c. han förlorar 1 hp ")
     Gandalf1=Gandalf1_hp - 1
+
+
+if (Gandalf1==1 and spelarens_val == "b"):
+    print("Din motståndare valde attack A och du valde attack B. Du förlorar 1 hp.")
+    spelarens_hp = spelarens_hp - 1
+elif(Gandalf1==2 and spelarens_val == "a"):
+    print("Din motsåndare valde attack b och dy valde attack a. Han förlorar 1 hp")
+    robot_hp=robot_hp - 1
+elif(Gandalf1==3 and spelarens_val == "c"):
+    print("din motståndare valde attack c och du valde attack c. ingen tar skada")
+elif(Gandalf1==2 and spelarens_val == "c"):
+    print("din motståndare valde attack b och du valde attack c. han förlorar 1 hp ")
+    robot_hp=robot_hp - 1
+elif(Gandalf1==1 and spelarens_val == "a"):
+    print ("din motståndare valde attack a och du valde attack a. ingen förlorar hp")
+elif(Gandalf1==2 and spelarens_val == "b"):
+    print("din motståndare valde attack b och vald attack ingen tar skada")
+elif(Gandalf1==3 and spelarens_val == "b"):
+    print("din motståndare valde attack c och du valde attack b. han tar skada")
+    robot_hp=robot_hp-1
+elif(Gandalf1==1 and spelarens_val == "c"):
+    print("din motståndare valde attack a och du valde attack c. han tar skada")
+    robot_hp=robot_hp-1
+elif(Gandalf1==3 and spelarens_val == "a"):
+    print("din motståndare valde attack c och du valde attack a. han tar skada")
+    robot_hp=robot_hp-1
